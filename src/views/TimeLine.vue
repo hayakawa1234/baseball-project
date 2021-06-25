8e883379eaee20c4954be58869f398c4bc81a605
shirozuka <85152097+shirozuka@users.noreply.github.com>
2021-06-25 18:04:10 +0900

    TimeLineページの更新
    投稿の枠組みを一旦作成

---
+++ src/views/TimeLine.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home-container">
+    <PostFrame />
+  </div>
+</template>
+
+<script>
+import PostFrame from "@/components/PostFrame.vue";
+export default {
+  name: "TimeLine",
+  components: {
+    PostFrame
+  }
+};
+</script>
+
+<style lang="scss">
+.home-container {
+  margin-left: 30px;
+  margin-right: 30px;
+}
+.description-area {
+  display: flex;
+}
+</style>
